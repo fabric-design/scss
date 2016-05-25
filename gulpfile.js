f232bffbcfc6144c24648018f0ee70b1380546c8
Fabian Weber <fabian.weber@zalando.de>
2016-05-25 16:44:06 +0200

    added icon generation task

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+/**
+ *  Welcome to your gulpfile!
+ *  The gulp tasks are splitted in several files in the gulp directory
+ *  because putting all here was really too long
+ */
+
+'use strict';
+
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var iconfont = require('gulp-iconfont');
+var iconfontCss = require('gulp-iconfont-css');
+var fs = require('fs');
+var path = require('path');
+
+gulp.task('icons', function(){
+	var iconsFoldersPath = 'icons/svg/';
+	var folders = getFolders(iconsFoldersPath);
+
+	var tasks = folders.map(function(folder) {
+		var fontName = folder.split('/').pop().replace(' ','-').toLowerCase();
+		return gulp.src(path.join(iconsFoldersPath, folder, '/**/*.svg'))
+		.pipe(iconfontCss({
+			fontName: fontName
+		}))
+		.pipe(iconfont({
+			fontName: fontName
+		}))
+		.pipe(gulp.dest('icons/'));
+	});
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch('icons/svg/**/*.{svg}', {
+    ignoreInitial: false
+  }, ['icons']);
+});
+
+
+// utils
+function getFolders(dir) {
+    return fs.readdirSync(dir)
+      .filter(function(file) {
+        return fs.statSync(path.join(dir, file)).isDirectory();
+      });
+}
+
+var getFolderName = function(file) {
+    var filePath = file.history[0]
+    .split('/');
+	return filePath[filePath.length - 2];
+}
